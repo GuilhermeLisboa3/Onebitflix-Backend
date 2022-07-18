01ef32fb023e8a6ba98202551ef69f49a8684d58
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2022-07-18 13:54:58 -0300

    Criando a tabela de curso e fazendo a relacionamento entre models

---
+++ src/models/Course.ts
@@ -0,0 +1,50 @@
+// src/models/Course.ts
+
+import { sequelize } from '../database'
+import { DataTypes, Model, Optional } from 'sequelize'
+
+export interface CourseAttributes {
+  id: number
+  name: string
+  synopsis: string
+  thumbnailUrl: string
+  featured: boolean
+  categoryId: number
+}
+
+export interface CourseCreationAttributes
+  extends Optional<CourseAttributes, 'id' | 'thumbnailUrl' | 'featured' > {}
+
+export interface CourseInstance
+  extends Model<CourseAttributes, CourseCreationAttributes>, CourseAttributes {}
+
+export const Course = sequelize.define<CourseInstance, CourseAttributes>('courses', {
+  id: {
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+    type: DataTypes.INTEGER
+  },
+  name: {
+    allowNull: false,
+    type: DataTypes.STRING
+  },
+  synopsis: {
+    allowNull: false,
+    type: DataTypes.TEXT
+  },
+  thumbnailUrl: {
+    type: DataTypes.STRING
+  },
+  featured: {
+    defaultValue: false,
+    type: DataTypes.BOOLEAN
+  },
+  categoryId: {
+    allowNull: false,
+    type: DataTypes.INTEGER,
+    references: { model: 'categories', key: 'id' },
+    onUpdate: 'CASCADE',
+    onDelete: 'RESTRICT'
+  }
+})

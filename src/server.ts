fdb68cf698ec8d73f2b772db905e97bd839b047d
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2022-07-18 13:43:26 -0300

    Adicionando resources no adminjs

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,19 @@
-import { sequelize } from './database/index';
 import express from 'express'
-import {adminJs ,adminJsRouter} from "./adminjs"
+import { sequelize } from './database'
+import { adminJs, adminJsRouter } from './adminjs'
 
 const app = express()
 
-app.use(adminJs.options.rootPath, adminJsRouter)
 app.use(express.static('public'))
+
+app.use(adminJs.options.rootPath, adminJsRouter)
 
 const PORT = process.env.port || 3000
 
-app.listen(PORT, () => {
-  sequelize.authenticate().then(()=>{
-    console.log('BD connection successfull')
+app.listen(PORT, async () => {
+  await sequelize.authenticate().then(() => {
+    console.log('DB connection successfull.')
   })
-  console.log(`Server started successfuly at port ${PORT}`)
+
+  console.log(`Server started successfuly at port ${PORT}.`)
 })

893e0c479fd86957805308693d718c84f245815e
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2022-07-19 15:53:24 -0300

    Incluindo paginção de categorias

--- src/controllers/categoriesController.ts
+++ src/controllers/categoriesController.ts
@@ -1,18 +1,20 @@
 import { Request, Response } from "express";
+import { json } from "stream/consumers";
+import { getPaginationParams } from "../helpers/getPaginationParams";
 import { Category } from "../models";
+import { categoryService } from "../services/categoryService";
 
 export const categoriesController = {
-    index: async (req:Request, res:Response)=>{
-        try {
-            const categories = await Category.findAll({
-                order:[['position', 'ASC']]
-            })
-            return res.json(categories)
-        } catch (error) {
-            if(error instanceof Error){
-                return res.status(400).json({message: error.message})
-            }
-        }
-        
+  index: async (req: Request, res: Response) => {
+    const [page, perPage] = getPaginationParams(req.query)
+    try {
+      const paginatedCategories = await categoryService.findAllPagineted(page,perPage)
+
+      return res.json(paginatedCategories)
+    } catch (error) {
+      if (error instanceof Error) {
+        return res.status(400).json({ message: error.message });
+      }
     }
-}
+  },
+};

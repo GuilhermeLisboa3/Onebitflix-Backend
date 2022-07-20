a5532e80daba8a65acafe00c332709da34c598f7
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2022-07-20 16:12:41 -0300

    Registro de usuários

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,12 @@
 import express from "express";
+import { authController } from "./controllers/authController";
 import { categoriesController } from "./controllers/categoriesController";
 import { coursesController } from "./controllers/coursesController";
 import { episodesController } from "./controllers/episodesController";
 const router = express.Router();
+
+//rotas para o user
+router.post('/auth/register',authController.resgiter)
 
 //rotas de categorias
 router.get("/categories", categoriesController.index);
